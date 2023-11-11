7e477b1d9a5ec7b76cf3f29562506458829e83fb
Shyam <dwivedisshyam@gmail.com>
2023-11-11 23:12:57 +0530

    remove html files

--- pkg/widget/button.go
+++
@@ -1,7 +0,0 @@
-package widget
-
-import "fyne.io/fyne/v2/widget"
-
-func NewButton(label string, onClick func()) *widget.Button {
-	return widget.NewButton(label, onClick)
-}
--- pkg/widget/radio.go
+++
@@ -1,9 +0,0 @@
-package widget
-
-import (
-	"fyne.io/fyne/v2/widget"
-)
-
-func NewRadioGroup(options []string, onChange func(string)) *widget.RadioGroup {
-	return widget.NewRadioGroup(options, onChange)
-}
